2b092d62d76856b99b9a6544a92bbdebff456f16
Fazeel Khalid <fazeel.k.khalid@gmail.com>
2024-11-20 11:10:13 -0800

    Cors middleware implemented :)

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Http\Request;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        $allowedOrigins = [
+            'https://admin.probusiness101.com',
+        ];
+
+        $origin = $request->headers->get('Origin');
+
+        if (in_array($origin, $allowedOrigins)) {
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+        }
+
+        return $response;
+    }
+}
