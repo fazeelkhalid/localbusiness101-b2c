dcaed9be5f8651810b0fba7e9e496261b5bc4ca8
Fazeel Khalid <fazeel.k.khalid@gmail.com>
2025-03-11 21:28:33 +0500

    Cors req done.

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,24 +1,38 @@
 <?php
 namespace App\Http\Middleware;
+
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        $response = $next($request);
-
         $allowedOrigins = [
             'https://admin.probusiness101.com',
         ];
 
         $origin = $request->headers->get('Origin');
 
+        $headers = [
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization',
+        ];
+
         if (in_array($origin, $allowedOrigins)) {
-            $response->headers->set('Access-Control-Allow-Origin', $origin);
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+            $headers['Access-Control-Allow-Origin'] = $origin;
+        }
+
+        // Handle Preflight Request
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
         }
 
         return $response;
